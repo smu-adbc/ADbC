d35e7263c374bf7ea188202247a61b46013cca16
draevin <draevinl@gmail.com>
2018-02-06 23:52:38 -0800

    Setup demo for showing dynamic rendering of partial view using model data, passing view input to load method in JQuery

---
+++ ADbC/Controllers/DatabaseDesignController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ADbC.Models;
+
+namespace ADbC.Controllers
+{
+    public class DatabaseDesignController : Controller
+    {
+        public ActionResult ERNotation()
+        {            
+            return View();
+        }
+
+        public ActionResult ShowERNotationOutput(int rel, int not)
+        {
+            ERNotationModel model;
+
+            //normall here we'd get the database data and pass
+            //these in to select from that list, but it's all
+            //hard coded for now, just testing
+            if ((rel is int r) && (not is int n))
+            {
+                model = new ERNotationModel(r, n);
+                return PartialView("ERNotationOutputPartial", model);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ ADbC/Models/ERNotationModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ADbC.Models
+{
+    public class ERNotationModel
+    {
+        public int currentRelation;
+        public int currentNotation;
+
+        List<ERNotationObject> ERList;
+
+        public ERNotationModel(int selRelation, int selNotation)
+        {
+            currentRelation = selRelation;
+            currentNotation = selNotation;
+
+            ERList = new List<ERNotationObject>
+            {
+                new ERNotationObject(1,1),
+                new ERNotationObject(1,2),
+                new ERNotationObject(2,1),
+                new ERNotationObject(2,2)
+            };
+        }
+
+        public string CurrentToString
+        {
+            get
+            {
+                ERNotationObject current = ERList.Find(x => x.RelationID == currentRelation && x.NotationID == currentNotation);
+                return current.Output;
+            }            
+        }
+    }
+
+    public class ERNotationObject
+    {
+        public int RelationID;
+        public int NotationID;
+        public string Output;
+
+        public ERNotationObject(int rel, int not)
+        {
+            RelationID = rel;
+            NotationID = not;
+            Output = $"Relation: {RelationID} -- Notation: {NotationID}";
+        }
+    }
+}
