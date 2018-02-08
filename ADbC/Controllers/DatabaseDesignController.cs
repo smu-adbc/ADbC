5653cb5cbcc7ff2fb7a04e0414efa8b3d49a7af3
Spencer Phillips <Spencer.G.Phillips@gmail.com>
2018-02-08 15:05:12 -0800

    Buttons
    I did a lot of things, but the biggest change was adding buttons and starting to figure out the controllers.

--- ADbC/Controllers/DatabaseDesignController.cs
+++ ADbC/Controllers/DatabaseDesignController.cs
@@ -14,11 +14,41 @@ namespace ADbC.Controllers
             return View();
         }
 
+        public ActionResult ScenarioToER()
+        {
+            return View();
+        }
+
+        public ActionResult ER_ToTables()
+        {
+            return View();
+        }
+
+        public ActionResult FunctionalDependencies()
+        {
+            return View();
+        }
+
+        public ActionResult Normalization()
+        {
+            return View();
+        }
+
+        public ActionResult Denormalization()
+        {
+            return View();
+        }
+
+        public ActionResult Anomalies()
+        {
+            return View();
+        }
+
         public ActionResult ShowERNotationOutput(int rel, int not)
         {
             ERNotationModel model;
 
-            //normall here we'd get the database data and pass
+            //normally here we'd get the database data and pass
             //these in to select from that list, but it's all
             //hard coded for now, just testing
             if ((rel is int r) && (not is int n))
