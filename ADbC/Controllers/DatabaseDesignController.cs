16a1056ff4e775006b7828f31e1bbf5a09696197
draevin <draevinl@gmail.com>
2018-02-18 02:33:04 -0800

    Adding Scenario To ER images and starting development on reusable multiple choice structure

--- ADbC/Controllers/DatabaseDesignController.cs
+++ ADbC/Controllers/DatabaseDesignController.cs
@@ -26,7 +26,17 @@ namespace ADbC.Controllers
 
         public ActionResult ScenarioToER()
         {
-            return View();
+            ERMultipleChoiceModelDataContext MCdc = new ERMultipleChoiceModelDataContext();
+            MCdc.ObjectTrackingEnabled = false;
+
+            MCdc.module = MCdc.SelectModuleByName("Scenario To ER").ToList().First();
+            MCdc.sections = MCdc.SelectModuleIntroSectionsByModuleID(MCdc.module.ModuleID).OrderBy(x => x.SectionOrder).ToList();
+            MCdc.contents = MCdc.SelectModuleIntroContentByModuleID(MCdc.module.ModuleID).OrderBy(x => x.ContentOrder).ToList();
+
+            MCdc.Question = MCdc.SelectERQuestionByDescShort("employees").ToList().First();
+            MCdc.Answers = MCdc.SelectERAnswersByQuestionID(MCdc.Question.ERQuestionID).ToList();
+
+            return View(MCdc);
         }
 
         public ActionResult ERToTables()
---
+++ ADbC/Models/ERMultipleChoiceModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ADbC.Models
+{
+    public partial class ERMultipleChoiceModelDataContext : ModuleBaseModelDataContext
+    {
+        public keysERQuestion Question;
+        public List<keysERAnswer> Answers;
+    }
+}
