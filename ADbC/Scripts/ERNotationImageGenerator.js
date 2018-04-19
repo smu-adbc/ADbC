db5176cd4baee93be5936964e861e08490280549
draevin <draevinl@gmail.com>
2018-04-18 22:23:18 -0700

    Added dual-notation function to ERNotation view, still needs beautifying

--- ADbC/Scripts/ERNotationImageGenerator.js
+++ ADbC/Scripts/ERNotationImageGenerator.js
@@ -1,15 +1,37 @@
 ﻿var url;
 $(document).ready(function () {
+    var selNotation1 = document.getElementById("combo1").value;;
+    var selNotation2 = document.getElementById("combo2").value;;
+    var selRelation;
     $('#radioBox input:radio').click(function () {
-        var selNotation = $('input[name=notation]:checked').val();
-        var selRelation = $('input[name=relation]:checked').val();
-        if (typeof selNotation !== 'undefined' && typeof selRelation !== 'undefined') {
-            var url = '/Content/Images/ERNotation/' + selNotation + '/' + selRelation + '.png';
-            $('#diagram').attr('src', url)
-            if ($('#diagram').css('visibility') == 'hidden') {
-                $('#diagram').css('visibility', 'visible')
-            }
-            //$('#output').load(url, { not: selNotation, rel: selRelation });
-        }
+        selRelation = $('input[name=relation]:checked').val();
+        generateImage();
     })
+
+    document.getElementById("combo1").onchange = function () {
+        selNotation1 = document.getElementById("combo1").value;
+        generateImage();
+    };
+
+    document.getElementById("combo2").onchange = function () {
+        selNotation2 = document.getElementById("combo2").value;
+        generateImage();
+    };
+
+    function generateImage() {
+        if (typeof selNotation1 !== 'undefined' && typeof selRelation !== 'undefined') {
+            var url = '/Content/Images/ERNotation/' + selNotation1 + '/' + selRelation + '.png';
+            $('#diagram1').attr('src', url)
+            if ($('#diagram1').css('visibility') == 'hidden') {
+                $('#diagram1').css('visibility', 'visible')
+            }
+        }
+        if (typeof selNotation2 !== 'undefined' && typeof selRelation !== 'undefined') {
+            var url = '/Content/Images/ERNotation/' + selNotation2 + '/' + selRelation + '.png';
+            $('#diagram2').attr('src', url)
+            if ($('#diagram2').css('visibility') == 'hidden') {
+                $('#diagram2').css('visibility', 'visible')
+            }
+        }
+    }
 })
---
+++ ADbC/Scripts/ERNotationImageViewer.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $('#diagrams img').click(function () {
+        var diagram = $(this).attr('src');
+        if (typeof diagram !== 'undefined') {
+            $('#diagramImage').attr('src', diagram)
+            $('#imageView').modal('show');
+        }
+    })
+})
